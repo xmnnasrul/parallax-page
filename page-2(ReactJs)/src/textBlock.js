63145ac286cfc668e4dc309437a78afac8bd6d27
xmnnasrul <mygithub.nasrul@gmail.com>
2023-03-02 09:52:59 +0700

    add page-2

---
+++ page-2(ReactJs)/src/textBlock.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function TextBlock() {
+    return (
+        <div id="textblock" >
+            <div id="textblock-container" >
+                <h1 id="textblock-title">What is Firewatch?</h1>
+                <p id="textblock-content">
+                    The year is 1989.<br /><br />
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus impedit architecto aliquam repellendus atque adipisci itaque veniam corporis eaque rerum labore possimus, beatae id neque vitae minus, quis facilis aut voluptatum, ea omnis. Maxime vero consectetur delectus, quasi cum voluptatem harum est provident totam deserunt accusamus ducimus quam omnis eveniet? Veritatis temporibus sequi beatae doloremque inventore quas consectetur tenetur minus numquam ab fugit, optio earum iste, deleniti eum unde molestias accusantium, dicta nam dolorem libero iusto facilis excepturi soluta? Debitis alias, iure vero illo dolorum nisi quibusdam. Voluptas expedita ipsa perferendis error, facilis cumque incidunt cupiditate quos sint blanditiis, eveniet asperiores laboriosam maiores tempora repellat. Numquam tenetur inventore eaque. Quam, tempore hic! Nesciunt, dolores ipsam fuga quisquam harum rem ducimus inventore consequatur aperiam similique cum ea necessitatibus eos debitis delectus perferendis quo! Ea nemo alias voluptate culpa aspernatur consectetur eius magni consequatur? Iusto nostrum laborum cumque at fuga, dolor repellat debitis eaque. Eaque eligendi harum dolores tempora aspernatur temporibus reiciendis natus in corrupti fugiat tenetur, minus, commodi autem pariatur? Optio, impedit ex vel ipsum eos reprehenderit est tempora dolores minus nihil perspiciatis placeat molestias nobis, recusandae rem temporibus delectus quia quibusdam ullam expedita qui non earum. Ut necessitatibus quam officiis, minus recusandae similique velit beatae maiores. Quae nemo dolorem voluptas nobis, cupiditate exercitationem nam facere et alias quam, corporis molestias facilis aut magnam dolorum debitis.
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default TextBlock;
